53ec905a37d1381eb934f4faa056167bdd278e1e
Adolfo Builes <builes.adolfo@gmail.com>
2014-05-30 21:23:46 -0300

    Init

---
+++ index.js
@@ -0,0 +1,32 @@
+var Filter = require('broccoli-filter');
+var Leaf    = require('leaf');
+var path     = require('path');
+var escodegen = require('escodegen');
+
+module.exports = AmdTranform;
+
+AmdTranform.prototype = Object.create(Filter.prototype);
+AmdTranform.prototype.constructor = AmdTranform;
+
+function AmdTranform (inputTree, options) {
+  if (!(this instanceof AmdTranform)) return new AmdTranform(inputTree, options);
+
+  Filter.call(this, inputTree, options);
+  this.options = options || {};
+}
+
+AmdTranform.prototype.extensions = ['js'];
+AmdTranform.prototype.targetExtension = 'js';
+
+AmdTranform.prototype.processString = function (source, destDir) {
+  var leaf = new Leaf(destDir, source);
+
+  if (leaf.hasDefine && leaf.isAnonymous){
+    var name = path.basename(destDir, '.js');
+
+    leaf.deanonymize(name);
+    source = escodegen.generate(leaf.ast);
+  }
+
+  return source;
+};
